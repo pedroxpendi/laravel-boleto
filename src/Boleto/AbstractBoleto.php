d1ef4f08d0c12295982f0a9faea3db09e5313e9f
Pedro Wickert <pedrowickert98@gmail.com>
2024-06-28 15:11:06 -0300

    Rollback

--- src/Boleto/AbstractBoleto.php
+++ src/Boleto/AbstractBoleto.php
@@ -1364,8 +1364,7 @@ abstract class AbstractBoleto implements BoletoContract
      */
     public function getLogoBase64()
     {
-        return 'data:image/jpg' .
-            //. pathinfo($this->getLogo(), PATHINFO_EXTENSION) .
+        return 'data:image/' . pathinfo($this->getLogo(), PATHINFO_EXTENSION) .
             ';base64,' . base64_encode(file_get_contents($this->getLogo()));
     }
 
